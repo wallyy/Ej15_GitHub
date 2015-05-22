4933911eb3a38c544338ec1b736e82e63441b6cd
Usuario <Usuario@aula2-PC7>
2015-05-22 09:52:30 +0200

    Update clase Cliente ok

--- src/es/curso/model/entity/Cliente.java
+++ src/es/curso/model/entity/Cliente.java
@@ -3,5 +3,94 @@ package es.curso.model.entity;
 public class Cliente {
 
 	//programacion
+	private String nombre;
+	private String apellidos;
+	private String DNI;
+	private int id;
+	
+	
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Cliente(int id, String nombre, String apellidos, String dNI) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		DNI = dNI;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((DNI == null) ? 0 : DNI.hashCode());
+		result = prime * result
+				+ ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (DNI == null) {
+			if (other.DNI != null)
+				return false;
+		} else if (!DNI.equals(other.DNI))
+			return false;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellidos="
+				+ apellidos + ", DNI=" + DNI + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+
 	
 }
