e39372ea5d5b32ed5f936bd8d24891ea2bf4ba3f
Usuario <Usuario@aula2-PC7>
2015-05-22 12:12:40 +0200

    UPDATE DAOS -> AgregarCliente

--- src/es/curso/persistence/model/dao/ClienteDao.java
+++ src/es/curso/persistence/model/dao/ClienteDao.java
@@ -1,10 +1,12 @@
 package es.curso.persistence.model.dao;
 
-public class ClienteDao {
+import es.curso.model.entity.Cliente;
+
+public interface ClienteDao {
 
 	// por cada entity q creamos (clases que tendran reflejo en bbdd) hacemos un DAO?
 	
-	
+	public void create(Cliente cliente);
 	
 	
 }
--- src/es/curso/persistence/model/dao/jdbc/ClienteDaoJdbc.java
+++ src/es/curso/persistence/model/dao/jdbc/ClienteDaoJdbc.java
@@ -1,5 +1,20 @@
 package es.curso.persistence.model.dao.jdbc;
 
-public class ClienteDaoJdbc {
+import es.curso.model.entity.Cliente;
+import es.curso.persistence.model.dao.ClienteDao;
+
+public class ClienteDaoJdbc implements ClienteDao{
+// enlazo lenguaje java con sql
+	@Override
+	public void create(Cliente cliente) {
+		
+		// van las instrucciones para:
+//		1. conectar con la bbdd
+//		2. preparar las sentencia -sql- para agregar
+//		3. ejecutar la sentencia -sql- 
+//		4. ...puede haber mas
+//		5. cerrar conexión
+		
+	}
 
 }
