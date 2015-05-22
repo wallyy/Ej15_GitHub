2ccc3199932a734aab3f0cf0b5a6a431fb6aa007
Usuario <Usuario@aula2-PC7>
2015-05-22 11:57:38 +0200

    ADD DarAltaClienteController--> method - agregar

---
+++ src/es/curso/controllers/ejb/DarAltaClienteControllerEjb.java
@@ -0,0 +1,20 @@
+package es.curso.controllers.ejb;
+
+import es.curso.controllers.DarAltaClienteController;
+import es.curso.model.entity.Cliente;
+
+public class DarAltaClienteControllerEjb implements DarAltaClienteController{
+
+	@Override
+	public void agregar(Cliente cliente) {
+		
+/*		ACA SE PONE LA LOGICA DEL NEG PARA AGRER CLIENTE
+		1. Verificar Datos
+		2. Agregarlo ----> llamar o (agreg) a la capa DAO para q se de de alta
+		3. Enviar email al jefe de Obra
+		4. enviar un email al cliente */
+		
+		
+	}
+
+}
